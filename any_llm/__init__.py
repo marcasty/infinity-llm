0c329beca6982c5d6f6d3a2286d6ed2e5a36418a
marcasty <markacastellano2@gmail.com>
2024-06-25 22:42:33 -0700

    import embedding classes

--- any_llm/__init__.py
+++ any_llm/__init__.py
@@ -1,7 +1,8 @@
 from .utils import model_mapping, Provider, get_api_key
 from .client_factory import from_any, get_default_mode
 
-from .embed.client import embed_from_openai
+from .embed.client import embed_from_openai, AnyEmbedder, AsyncAnyEmbedder
+from .embed.client_factory import embed_from_any
 from .embed.client_voyage import embed_from_voyage
 from .embed.client_cohere import embed_from_cohere
 from .embed.client_mistral import embed_from_mistral
