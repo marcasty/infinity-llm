9784fc67d4774493e5826b08656acdd31b4452b5
marcasty <markacastellano2@gmail.com>
2024-06-25 17:14:11 -0700

    switch up imports a little

--- any_llm/__init__.py
+++ any_llm/__init__.py
@@ -1,4 +1,7 @@
 from .utils import model_mapping, Provider, get_api_key
 from .client_factory import from_any, get_default_mode
 
-from .embed.client import AnyEmbedder, AsyncAnyEmbedder
+from .embed.client import embed_from_openai
+from .embed.client_voyage import embed_from_voyage
+from .embed.client_cohere import embed_from_cohere
+from .embed.client_mistral import embed_from_mistral
--- any_llm/embed/client_factory.py
+++ any_llm/embed/client_factory.py
@@ -6,11 +6,12 @@ from mistralai.client import MistralClient
 from mistralai.async_client import MistralAsyncClient
 from voyageai import Client, AsyncClient
 
-from any_llm.embed import AnyEmbedder, AsyncAnyEmbedder, embed_from_cohere, embed_from_mistral, embed_from_openai, embed_from_voyage
+import any_llm
+from any_llm.embed import AnyEmbedder, AsyncAnyEmbedder
 from any_llm import Provider, get_api_key
 
 
-def from_any(
+def embed_from_any(
     provider: Provider,
     model_name: str,
     async_client: bool = False
@@ -30,16 +31,16 @@ def from_any(
 
     if provider == Provider.OPENAI:
         raw_client = AsyncOpenAI(api_key=api_key) if async_client else OpenAI(api_key=api_key)
-        return embed_from_openai(raw_client)
+        return any_llm.embed_from_openai(raw_client)
     elif provider == Provider.COHERE:
         raw_client = cohere.AsyncClient(api_key=api_key) if async_client else cohere.Client(api_key=api_key)
-        return embed_from_cohere(raw_client)
+        return any_llm.embed_from_cohere(raw_client)
     elif provider == Provider.VOYAGE:
         raw_client = AsyncClient(api_key=api_key) if async_client else Client(api_key=api_key)
-        return embed_from_voyage(raw_client)
+        return any_llm.embed_from_voyage(raw_client)
     elif provider == Provider.MISTRAL:
         raw_client = MistralAsyncClient(api_key=api_key) if async_client else MistralClient(api_key=api_key)
-        return embed_from_mistral(raw_client)
+        return any_llm.embed_from_mistral(raw_client)
     elif provider == Provider.ANYSCALE:
         raw_client = AsyncOpenAI(
             base_url="https://api.endpoints.anyscale.com/v1",
@@ -48,6 +49,6 @@ def from_any(
             base_url="https://api.endpoints.anyscale.com/v1",
             api_key=api_key,
         )
-        return embed_from_openai(raw_client)
+        return any_llm.embed_from_openai(raw_client)
     else:
         raise ValueError(f"Cannot create embedding client for unsupported provider: {provider}")
